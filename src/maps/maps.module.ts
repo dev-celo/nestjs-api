218bef2e8d9dabe8b926a884983a428c61fa095d
dev-celo <marcelohenrique.backend@gmail.com>
2024-12-11 18:32:31 -0300

    updt: Utilizando provider personalizado para integrar bibliotecas externas.

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
+import { Client as googleMapsClient } from '@googlemaps/google-maps-services-js';
 
 @Module({
   controllers: [PlacesController],
-  providers: [PlacesService]
+  providers: [
+    PlacesService,
+    {
+      provide: googleMapsClient,
+      useValue: new googleMapsClient(),
+    },
+  ],
 })
 export class MapsModule {}
