bf41d2aaeb5cf1cc25a3a2cd70a57b75e41e81a2
dev-celo <marcelohenrique.backend@gmail.com>
2024-12-12 08:53:13 -0300

    Feat: Implementando directions controller no módulo maps.

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
 import { Client as googleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
 
 @Module({
-  controllers: [PlacesController],
+  controllers: [PlacesController, DirectionsController],
   providers: [
     PlacesService,
     {
       provide: googleMapsClient,
       useValue: new googleMapsClient(),
     },
+    DirectionsService,
   ],
 })
 export class MapsModule {}
